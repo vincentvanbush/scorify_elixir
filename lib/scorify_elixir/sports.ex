bccb0fe00b95252ec423c01f3fb4259023b95469
Michał Buszkiewicz <embed1@gmail.com>
2018-08-16 15:53:58 +0200

    Creating league seasons, some work on adding sides to seasons

--- lib/scorify_elixir/sports.ex
+++ lib/scorify_elixir/sports.ex
@@ -1,9 +1,11 @@
 defmodule ScorifyElixir.Sports do
   alias ScorifyElixir.Repo
-  alias ScorifyElixir.Sports.{Sport, League}
+  alias ScorifyElixir.Sports.{Sport, League, Side, LeagueSeason}
   import Ecto
   require Ecto.Query
   import Ecto.Query
+
+  require IEx
 
   def list_sports do
     Sport |> Repo.all
@@ -27,19 +29,23 @@ defmodule ScorifyElixir.Sports do
     League |> Repo.all
   end
 
-  def list_leagues(sport) do
+  def list_leagues(sport = %Sport{}) do
     assoc(sport, :leagues) |> Repo.all
   end
 
-  def list_league_sides(league) do
+  def list_league_sides(league = %League{}) do
+    last_season = league |> league_last_season
+    last_season |> assoc(:sides) |> Repo.all
+  end
+
+  def league_last_season(league = %League{}) do
     league_seasons_query = league |> assoc(:league_seasons)
     last_season_query =
       from l in league_seasons_query,
       where: l.start_date <= from_now(0, "day"),
       order_by: [desc: :start_date],
       limit: 1
-    last_season = last_season_query |> Repo.one!
-    last_season |> assoc(:sides) |> Repo.all
+    last_season_query |> Repo.one!
   end
 
   def list_sport_sides(sport) do
@@ -48,5 +54,29 @@ defmodule ScorifyElixir.Sports do
 
   def create_league(sport, attrs \\ %{}) do
     sport |> build_assoc(:leagues, attrs) |> Repo.insert!
+  end
+
+  def create_side(sport = %Sport{}, attrs \\ %{}) do
+    sport
+    |> build_assoc(:sides, attrs)
+    |> Repo.insert!
+  end
+
+  def add_side_to_league(side = %Side{}, league: %League{} = league) do
+    last_season = league |> league_last_season
+    add_side_to_league_season(side, league_season: last_season)
+  end
+
+  def add_side_to_league_season(side = %Side{},
+                                league_season: (%LeagueSeason{} = league_season)) do
+    # TODO
+  end
+
+  def create_league_season(league, attrs = %{}) do
+    season =
+      league
+      |> build_assoc(:league_seasons)
+      |> LeagueSeason.changeset(attrs)
+    season |> Repo.insert!
   end
 end
--- lib/scorify_elixir/sports/league_season.ex
+++ lib/scorify_elixir/sports/league_season.ex
@@ -2,6 +2,7 @@ defmodule ScorifyElixir.Sports.LeagueSeason do
   use Ecto.Schema
   import Ecto.Changeset
 
+  require IEx
 
   schema "league_seasons" do
     field :end_date, :date
@@ -15,7 +16,27 @@ defmodule ScorifyElixir.Sports.LeagueSeason do
   end
 
   @doc false
+  def changeset(league_season,
+                attrs = %{start_date: start_date, end_date: end_date, name: name})
+                when is_nil(name) and not(is_nil(start_date) or is_nil(end_date)) do
+    start_year = Ecto.Date.cast!(start_date).year
+    end_year = Ecto.Date.cast!(end_date).year
+    make_changeset(league_season, attrs |> Map.put(:name, "#{start_year}/#{end_year}"))
+  end
+
+  def changeset(league_season, attrs = %{name: name}) when is_nil(name) do
+    make_changeset(league_season, attrs |> Map.put(:name, nil))
+  end
+
+  def changeset(league_season, attrs = %{name: _name}) do
+    make_changeset(league_season, attrs)
+  end
+
   def changeset(league_season, attrs) do
+    changeset(league_season, attrs |> Map.put_new(:name, nil))
+  end
+
+  defp make_changeset(league_season, attrs) do
     league_season
     |> cast(attrs, [:name, :start_date, :end_date])
     |> validate_required([:name, :start_date, :end_date])
