06b80f0404dcf0d5baa2687333b7f63baab2bca9
Michał Buszkiewicz <embed1@gmail.com>
2018-08-17 19:03:37 +0200

    Fix league season changeset when dates are invalid

--- lib/scorify_elixir/sports/league_season.ex
+++ lib/scorify_elixir/sports/league_season.ex
@@ -3,22 +3,21 @@ defmodule ScorifyElixir.Sports.LeagueSeason do
   import Ecto.Changeset
 
   schema "league_seasons" do
-    field :end_date, :date
-    field :name, :string
-    field :start_date, :date
+    field(:end_date, :date)
+    field(:name, :string)
+    field(:start_date, :date)
 
-    belongs_to :league, ScorifyElixir.Sports.League
-    many_to_many :sides, ScorifyElixir.Sports.Side, join_through: "sides_league_seasons"
+    belongs_to(:league, ScorifyElixir.Sports.League)
+    many_to_many(:sides, ScorifyElixir.Sports.Side, join_through: "sides_league_seasons")
 
     timestamps()
   end
 
   @doc false
-  def changeset(league_season,
-                attrs = %{start_date: start_date, end_date: end_date, name: name})
-                when is_nil(name) and not(is_nil(start_date) or is_nil(end_date)) do
-    start_year = Ecto.Date.cast!(start_date).year
-    end_year = Ecto.Date.cast!(end_date).year
+  def changeset(league_season, attrs = %{start_date: start_date, end_date: end_date, name: name})
+      when is_nil(name) and not (is_nil(start_date) or is_nil(end_date)) do
+    start_year = try do: Ecto.Date.cast!(start_date).year, rescue: (_ in Ecto.CastError -> nil)
+    end_year = try do: Ecto.Date.cast!(end_date).year, rescue: (_ in Ecto.CastError -> nil)
     make_changeset(league_season, attrs |> Map.put(:name, "#{start_year}/#{end_year}"))
   end
 
